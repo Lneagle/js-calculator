6bf480c1f4e8519362b6e366015d30a6740dd8e5
Lacey Murphy <lneagle@gmail.com>
2025-07-10 17:20:47 -0400

    Implemented HTML structure and initial calculator functionality

--- calculator.js
+++ calculator.js
@@ -33,43 +33,51 @@ function createResultObject(operand1, operand2, operator, result) {
     return obj;
 }
 
-function refreshResults() {
-    let lastResult = resultsArray[0];
+function processClick(choice) {
+    document.querySelector("#error").innerText = "";
+    let num1 = Number(document.querySelector("#firstNum").value);
+    let num2 = Number(document.querySelector("#secondNum").value);
+    let lastResult;
+    switch (choice) {
+        case "add":
+            add(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            divide(num1, num2);
+            break;
+    }
+    lastResult = resultsArray[0];
     document.querySelector("#latest p").innerText = `${lastResult.operands[0]} ${lastResult.operation} ${lastResult.operands[1]} = ${lastResult.result}`;
+    document.querySelector("#firstNum").value = "";
+    document.querySelector("#secondNum").value = "";
+}
 
+document.querySelector("#add").addEventListener("click", function() {
+    processClick("add");
+});
+document.querySelector("#subtract").addEventListener("click", function() {
+    processClick("subtract");
+});
+document.querySelector("#multiply").addEventListener("click", function() {
+    processClick("multiply");
+});
+document.querySelector("#divide").addEventListener("click", function() {
+    processClick("divide");
+});
+document.querySelector("#showHistory").addEventListener("click", function() {
     if (resultsArray.length > 1) {
         document.querySelector("#history p").innerText = "";
         for (let i=1; i<resultsArray.length; i++) {
             document.querySelector("#history p").innerText += `${resultsArray[i].operands[0]} ${resultsArray[i].operation} ${resultsArray[i].operands[1]} = ${resultsArray[i].result}\n`;
         }
+    } else {
+        document.querySelector("#history p").innerText = "There are no stored calculations."
     }
-}
-
-document.querySelector("#add").addEventListener("click", function() {
-    document.querySelector("#error").innerText = "";
-    add(Number(document.querySelector("#firstNum").value), Number(document.querySelector("#secondNum").value));
-    refreshResults();
-    document.querySelector("#firstNum").value = "";
-    document.querySelector("#secondNum").value = "";
-});
-document.querySelector("#subtract").addEventListener("click", function() {
-    document.querySelector("#error").innerText = "";
-    subtract(Number(document.querySelector("#firstNum").value), Number(document.querySelector("#secondNum").value));
-    refreshResults();
-    document.querySelector("#firstNum").value = "";
-    document.querySelector("#secondNum").value = "";
-});
-document.querySelector("#multiply").addEventListener("click", function() {
-    document.querySelector("#error").innerText = "";
-    multiply(Number(document.querySelector("#firstNum").value), Number(document.querySelector("#secondNum").value));
-    refreshResults();
-    document.querySelector("#firstNum").value = "";
-    document.querySelector("#secondNum").value = "";
-});
-document.querySelector("#divide").addEventListener("click", function() {
-    document.querySelector("#error").innerText = "";
-    divide(Number(document.querySelector("#firstNum").value), Number(document.querySelector("#secondNum").value));
-    refreshResults();
-    document.querySelector("#firstNum").value = "";
-    document.querySelector("#secondNum").value = "";
+    this.innerText = "Refresh calculation history";
 });
